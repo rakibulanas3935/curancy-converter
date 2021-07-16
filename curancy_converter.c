0a3e5fe8e7290d6f52e2bbeabe5690edcd69c34c
rakibulanas3935 <anas15-3935@diu.edu.bd>
2021-07-16 11:54:13 +0600

    first commit

---
+++ curancy_converter.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{   
+    int select_currancy;
+    float exchange_rate;
+    printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+    printf("+++++++++++++++++++++++++  CUARRANCY CONVERTER  +++++++++++++++++++++++\n");
+    printf("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+    // printf("\n\n\n\n\npress Enter to continue : ");
+    printf("\n\n\n\n Today exchange rate : ");
+    scanf("%f",&exchange_rate);
+    // currancy slection 
+
+    printf("Enter your cuurancy : ");   
+
+    printf ("<1> To Afganisthan AFN\n");
+    printf ("<2> To European euro EUR\n");
+    printf ("<3> To United States dollar USD\n");
+    printf ("<4> To Argentine peso ARS\n");
+    printf ("<5> To Riyal\n");
+    printf ("<6> To Dollar\n");
+    printf ("<7> To Euro\n");
+    printf ("<8> To Rupees\n");
+    printf ("<9> To Dinar\n");
+    printf ("<10> To Riyal\n");
+    printf ("<11> To Dollar\n");
+    printf ("<12> To Euro\n");
+    printf ("<13> To Rupees\n");
+    printf ("<14> To Dinar\n");
+    printf ("<15> To Riyal\n");
+    printf ("<16> To Dollar\n");
+    printf ("<17> To Euro\n");
+    printf ("<18> To Rupees\n");
+    printf ("<19> To Dinar\n");
+    printf ("<20> To Riyal\n");
+    printf ("<21> To Dollar\n");
+    printf ("<22> To Euro\n");
+    printf ("<23> To Rupees\n");
+    printf ("<24> To Dinar\n");
+    printf ("<25> To Riyal\n");
+    printf ("<26> To Dollar\n");
+    printf ("<27> To Euro\n");
+    printf ("<28> To Rupees\n");
+    printf ("<29> To Dinar\n");
+    printf ("<30> To Riyal\n");
+    printf ("<31> To Dollar\n");
+    printf ("<32> To Riyal\n");
+    printf ("<33> To Dollar\n");
+    printf ("<34> To Riyal\n");
+    printf ("<35> To Dollar\n");
+    printf ("<36> To Riyal\n");
+    printf ("<37> To Dollar\n");
+    printf ("<38> To Riyal\n");
+    printf ("<39> To Dollar\n");
+    printf ("<40> To Dollar\n");
+    printf ("<41> To Dollar\n");
+    printf ("<42> To Dollar\n");
+    printf ("<43> To Dollar\n");
+    printf ("<44> To Dollar\n");
+    printf ("<45> To Dollar\n");
+    printf ("<46> To Dollar\n");
+    printf ("<47> To Dollar\n");
+    printf ("<48> To Dollar\n");
+    printf ("<49> To Dollar\n");
+    printf ("<50> To Dollar\n");
+    scanf("%d",&select_currancy);
+    
+
+}
